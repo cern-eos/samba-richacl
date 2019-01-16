4b0c293c096a755a7631643f6cc3b3938e804876
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2019-01-16 14:45:51 +0100

    Fixes to make the CI compile

--- samba_richacl_build.sh
+++ samba_richacl_build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-yum install -y rpm-build yum-utils librichacl librichacl-devel
-
 sambaspec=~/rpmbuild/SPECS/samba.spec
 sambabuild=$(ls -d ~/rpmbuild/BUILD/samba-[0-9.]*[0-9] 2>/dev/null)
 if [[ ! -d $sambabuild ]]; then
@@ -9,16 +7,16 @@ if [[ ! -d $sambabuild ]]; then
   yumdownloader --source samba
   
   rpm -i samba-*.src.rpm
-  yum-builddep -y ~/rpmbuild/SPECS/samba.spec
+  yum-builddep -y $sambaspec
 
   if false; then		# brute force: full, lengthy build
-    rpmbuild -bc ~/rpmbuild/SPECS/samba.spec
+    rpmbuild -bc $sambaspec
   else
     # an alternative to the full "rpmbuild ~/rpmbuild/SPECS/samba.spec"  
-    rpmbuild -bp ~/rpmbuild/SPECS/samba.spec
+    rpmbuild -bp $sambaspec
     sambabuild=$(ls -d ~/rpmbuild/BUILD/samba-[0-9.]*[0-9])
     sed -i -e '/^all:/ a \\t@echo not running make \nall2:' $sambabuild/Makefile	# stop after configure
-    rpmbuild -bc --short-circuit ~/rpmbuild/SPECS/samba.spec || exit			# do the configure without build
+    rpmbuild -bc --short-circuit $sambaspec || exit			# do the configure without build
   fi
   
 fi
