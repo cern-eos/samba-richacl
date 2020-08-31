dc362c83c16aa2e191c07a631c5603412d5046a7
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2020-08-31 16:52:04 +0200

    Attempt to build on C8

---
+++ gitlab-ci/publish_rpms.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+kinit stci@CERN.CH -k -t /stci.krb5/stci.keytab
+
+export_rpms() {   # args: source targetarch
+    EXPORT_DIR=/eos/project/s/storage-ci/www/cernbox/tag/$2
+    echo "Publishing from $1 in location: ${EXPORT_DIR}"
+    mkdir -p ${EXPORT_DIR}/SRPMS ${EXPORT_DIR}/x86_64
+    cp $1/RPMS/*src.rpm ${EXPORT_DIR}/SRPMS/
+    cp $1/RPMS/*x86_64.rpm ${EXPORT_DIR}/x86_64/
+    createrepo -q ${EXPORT_DIR}/x86_64/
+}
+
+export_rpms RPMS_cc7 el-7
+export_rpms RPMS_c8  el-8
+
