431ea7cd5852a054e2bc5d57cd3420e8817ae98f
Rainer Toebbicke <tobbicke@mail.cern.ch>
2020-11-16 10:38:32 +0100

    build samba from tgz

--- samba_richacl_build.sh
+++ samba_richacl_build.sh
@@ -9,8 +9,68 @@
 # . rpm -i the newly created rpm
 
 
-sambaver=${1-samba}		# e.g. samba-4.9.1-6.el7
+sambaver=${1-samba}		# e.g. samba-4.9.1-6.el7, samba-4.12.10 for "tgz" build
 dir=$(dirname $0)
+
+sambaspec=~/rpmbuild/SPECS/samba.spec
+
+build_from_tgz() {      # download samba.tar.gz and create srpm
+
+set -x
+    # decent place to find seminal samba spec files in fedora, e.g.
+    # https://dl.fedoraproject.org/pub/fedora/linux/development/rawhide/Everything/source/tree/Packages/s/samba-4.12-10.fc34.src.rpm
+    sver=${sambaver##samba-}
+    iniSpec="$3"
+    [[ -n "$3" ]] || { 
+        case $sver in 
+            4.11.*)  iniSpec=samba.spec.4.11.x ;;
+            4.12.*)  iniSpec=samba.spec.4.12 ;;
+            *) echo Error, need initial spec file; exit 1 
+                ;;
+        esac
+    }
+
+    cp $iniSpec $sambaspec || { echo Error, failed to copy initial spec file; exit 1; }
+
+
+    sed -i -r \
+            -e "/(global|define) samba_version .*/s//\1 samba_version ${sver}/" \
+            -e"/xzcat/s//zcat/"  \
+            -e "/widelinks./d" \
+            -e "/dnsresolver./d" \
+            -e "/exclude .*mandir.*vfs_ceph/d" \
+            -e "/gp_scripts_ext./d" \
+            $sambaspec
+
+    sed -i  \
+            -e "/{_make_verbose}/s//{?_make_verbose}/" \
+            $sambaspec
+
+    #sed -i -r \
+    #        -e "/_bindir.\/winexe/a%{_mandir}/man1/winexe.1.gz\\n" \
+    #        $sambaspec
+
+
+    [[ $sver < 4.13.0 ]] && {
+        sed -i -e '/global required_mit_krb5 .*/s//global required_mit_krb5 1.17/' $sambaspec
+        sed -i -e '/global ldb_version .*/s//global ldb_version 2.1.4/' $sambaspec
+    }
+
+
+
+    # download the sources and build srpm
+    rpmbuild --undefine=_disable_source_fetch -bs $sambaspec || exit
+    echo now run:  rpmbuild -bb $sambaspec
+
+    exit 0
+}
+
+
+
+
+
+
+
 
 [[ -n "$2" ]] && {
     case $2 in 
@@ -21,13 +81,18 @@ dir=$(dirname $0)
             rpmbuild -bs ~/rpmbuild/SPECS/samba_richacl.spec
             exit;;
 
+        tgz)            # download samba.tar.gz and create srpm, need seminal spec file
+            # e.g.: $0 samba-4.12.10 tgz ./samba.initial.spec
+            build_from_tgz $*
+            exit;;
+
         *)
             echo "'$2' invalid"
             exit 1;;
     esac
 }
 
-sambaspec=~/rpmbuild/SPECS/samba.spec
+
 
 if [[ $sambaver = samba ]]; then
     sambabuild=$(ls -d ~/rpmbuild/BUILD/samba-[0-9.]*[0-9] 2>/dev/null)
